866a37362013fcb284f76c82cb39a94c842635c0
aryama02 <mahadihossainaryan@gmail.com>
2025-12-27 09:36:09 -0600

    feat: add user subscription and unsubscription service functions.

--- services/payment.py
+++ services/payment.py
@@ -9,6 +9,19 @@ async def subscribe_user_service(email: str):
     db_instance = get_database()
     users_collection = db_instance.get_collection("users")
     user_subscription = db_instance.get_collection("subscriptions")
+   
+   #Renew Subscription if expired 
+    existing_subscription = await user_subscription.find_one({"user_email": email})
+    if existing_subscription:
+        subscription_end_date = existing_subscription["subscription_end_date"]
+        if subscription_end_date < datetime.utcnow():
+            await user_subscription.update_one(
+                {"user_email": email}, {"$set": {"subscription_end_date": datetime.utcnow() + timedelta(days=30)}}
+            )
+            return {"message": "User subscription renewed successfully"}
+    
+        
+
     result = await users_collection.update_one(
         {"email": email}, {"$set": {"isSubscribed": True}}
     )
@@ -22,3 +35,18 @@ async def subscribe_user_service(email: str):
         return {"message": "User subscribed successfully"}
     else:
         return {"message": "User not found or already subscribed"}
+
+
+async def unsubscribe_user_service(email: str):
+    db_instance = get_database()
+    users_collection = db_instance.get_collection("users")
+    user_subscription = db_instance.get_collection("subscriptions")
+    result = await users_collection.update_one(
+        {"email": email}, {"$set": {"isSubscribed": False}}
+    )
+    if result.modified_count == 1:
+        await user_subscription.delete_one({"user_email": email})
+        return {"message": "User unsubscribed successfully"}
+    else:
+        return {"message": "User not found or not subscribed"}
+
